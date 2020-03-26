e33a0f9feaf128e4a37e47016ed14372630dfcf0
Yang123321 <fujsjssjsj@163.com>
2020-03-26 18:53:10 +0800

    add CodeGen.hs file

---
+++ src/Language/CodeGen.hs
@@ -0,0 +1,38 @@
+module Language.CodeGen where
+
+import           Control.Monad.Trans.Except
+import           Language.CoreErlang.Syntax
+import qualified Language.PureScript.CoreFn   as C
+import           Language.PureScript.PSString (decodeStringWithReplacement)
+import           Prelude
+
+type Result = Except String Expr
+
+corexpr2expr :: C.Expr C.Ann -> Result
+corexpr2expr expr = undefined
+
+
+
+corelit2expr :: C.Literal (C.Expr C.Ann) -> Result
+corelit2expr (C.NumericLiteral (Left i)) = return $  Lit $ LInt i
+corelit2expr (C.StringLiteral ppstring) = return $ Lit $ LString $ decodeStringWithReplacement ppstring
+corelit2expr (C.CharLiteral c) = return $ Lit $ LChar c
+corelit2expr (C.BooleanLiteral True) = return $ Lit $ LAtom $ Atom "true"
+corelit2expr (C.BooleanLiteral False) = return $ Lit $ LAtom $ Atom "false"
+corelit2expr (C.ArrayLiteral xs) = do
+  res <- mapM corexpr2expr xs
+  -- 完全没懂是什么意思。。。。。。。
+  return $ List $ L $ [Exprs $ Ann undefined undefined]
+corelit2expr x = undefined
+
+
+
+
+
+
+
+
+
+
+
+
