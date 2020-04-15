a1baa58751a970da5b770cab1a4347226abe7743
Feng Lee <feng@emqx.io>
2020-04-15 21:20:43 +0800

    Update copyright

--- app/Version.hs
+++ app/Version.hs
@@ -1,10 +1,11 @@
 -----------------------------------------------------------------------------
 -- |
 -- Module      :  Version
--- Copyright   :  (c) Feng Lee 2020
+-- Copyright   :  (c) 2020 EMQ Technologies Co., Ltd.
 -- License     :  BSD-style (see the LICENSE file)
 --
 -- Maintainer  :  Feng Lee, feng@emqx.io
+--                Yang M, yangm@emqx.io
 -- Stability   :  experimental
 -- Portability :  portable
 --
--- lib/System/IO.erl
+++ lib/System/IO.erl
@@ -1,3 +1,17 @@
+%%---------------------------------------------------------------------------
+%% |
+%% Module      :  IO
+%% Copyright   :  (c) 2020 EMQ Technologies Co., Ltd.
+%% License     :  BSD-style (see the LICENSE file)
+%%
+%% Maintainer  :  Feng Lee, feng@emqx.io
+%%                Yang M, yangm@emqx.io
+%% Stability   :  experimental
+%% Portability :  portable
+%%
+%% The Erlang IO Module.
+%%
+%%---------------------------------------------------------------------------
 -module('IO').
 
 -export([print/1]).
