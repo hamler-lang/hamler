a8574501f57a378cb661981fac70be4b90ac43e5
Feng Lee <feng@emqx.io>
2020-04-15 17:45:29 +0800

    Update Semigroup module

--- lib/Data/Semigroup.erl
+++ lib/Data/Semigroup.erl
@@ -20,7 +20,7 @@
 
 %% stringAppend :: String -> String -> String
 -spec(stringAppend(string(), string()) -> string()).
-stringAppend(S1, S2) -> listAppend(S1, S2).
+stringAppend(S1, S2) -> lists:append(S1, S2).
 
 %% listAppend :: forall a. List a -> List a -> List a
 -spec(listAppend(list(), list()) -> list()).
