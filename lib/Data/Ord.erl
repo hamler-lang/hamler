637bc2f2a12d2c0e8c0b51bab0148c62b952bbf1
Feng Lee <feng@emqx.io>
2020-04-15 19:08:11 +0800

    Update the Ord.erl and Ord.core

--- lib/Data/Ord.erl
+++ lib/Data/Ord.erl
@@ -14,31 +14,31 @@
 %%---------------------------------------------------------------------------
 -module('Ord').
 
--export([ compBool/5
-        , compChar/5
-        , compInt/5
-        , compFloat/5
-        , compNum/5
-        , compString/5
+-export([ compBool/3
+        , compChar/3
+        , compInt/3
+        , compFloat/3
+        , compNum/3
+        , compString/3
         ]).
 
-compBool(LT, GT, EQ, B1, B2) ->
-    comp(LT, GT, EQ, B1, B2).
+compBool(LT, GT, EQ) ->
+    fun(B1, B2) -> comp(LT, GT, EQ, B1, B2) end.
 
-compChar(LT, GT, EQ, C1, C2) ->
-    comp(LT, GT, EQ, C1, C2).
+compChar(LT, GT, EQ) ->
+    fun (C1, C2) -> comp(LT, GT, EQ, C1, C2) end.
 
-compInt(LT, GT, EQ, I1, I2) ->
-    comp(LT, GT, EQ, I1, I2).
+compInt(LT, GT, EQ) ->
+    fun(I1, I2) -> comp(LT, GT, EQ, I1, I2) end.
 
-compFloat(LT, GT, EQ, F1, F2) ->
-    comp(LT, GT, EQ, F1, F2).
+compFloat(LT, GT, EQ) ->
+    fun(F1, F2) -> comp(LT, GT, EQ, F1, F2) end.
 
-compNum(LT, GT, EQ, N1, N2) ->
-    comp(LT, GT, EQ, N1, N2).
+compNum(LT, GT, EQ) ->
+    fun(N1, N2) -> comp(LT, GT, EQ, N1, N2) end.
 
-compString(LT, GT, EQ, S1, S2) ->
-    comp(LT, GT, EQ, S1, S2).
+compString(LT, GT, EQ) ->
+    fun(S1, S2) -> comp(LT, GT, EQ, S1, S2) end.
 
 -compile({inline, [comp/5]}).
 comp(LT, GT, EQ, X, Y) ->
