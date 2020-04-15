8dbf3da69977e52e43d7644452da995b1b749100
Feng Lee <feng@emqx.io>
2020-04-15 15:45:06 +0800

    Fix build errors

---
+++ lib/Data/Binary.erl
@@ -0,0 +1,15 @@
+%%---------------------------------------------------------------------------
+%% |
+%% Module      :  Binary
+%% Copyright   :  (c) 2020 EMQ Technologies Co., Ltd.
+%% License     :  BSD-style (see the LICENSE file)
+%%
+%% Maintainer  :  Feng Lee, feng@emqx.io
+%%                Yang M, yangm@emqx.io
+%% Stability   :  experimental
+%% Portability :  portable
+%%
+%% The Binary Module.
+%%
+%%---------------------------------------------------------------------------
+-module('Binary').
