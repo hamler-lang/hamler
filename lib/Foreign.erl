e2e71bbbe8c6f3b6fb52386537dbeb779f66066c
Feng Lee <feng@emqx.io>
2020-04-23 08:50:11 +0800

    Update Foreign.erl
    - Add 'ffi/2', 'ffiIO/2' functions

--- lib/Foreign.erl
+++ lib/Foreign.erl
@@ -14,17 +14,39 @@
 %%---------------------------------------------------------------------------
 -module('Foreign').
 
--export([ffi/3, ffiIO/3]).
+-export([ ffi/2
+        , ffi/3
+        , ffiIO/2
+        , ffiIO/3
+        ]).
+
+-spec(ffi(string(), string()) -> fun()).
+ffi(Mod, Fun) ->
+    {Fun1, Arity} = split(Fun),
+    ffi(Mod, Fun1, Arity).
 
 -spec(ffi(string(), string(), arity()) -> fun()).
 ffi(Mod, Fun, Arity) when Arity >= 0 ->
     curry(atom(Mod), atom(Fun), [], Arity).
 
+-spec(ffiIO(string(), string()) -> fun()).
+ffiIO(Mod, Fun) -> ffi(Mod, Fun).
+
 -spec(ffiIO(string(), string(), arity()) -> fun()).
 ffiIO(Mod, Fun, Arity) -> ffi(Mod, Fun, Arity).
 
-curry(M, F, A, 0) -> erlang:apply(M, F, A);
+curry(M, F, A, 0) -> erlang:apply(M, F, lists:reverse(A));
 curry(M, F, A, N) -> fun(X) -> curry(M, F, [X|A], N-1) end.
 
+-compile({inline, [split/1]}).
+split(S) ->
+    case string:split(S, "/") of
+        [F, A] -> {F, int(A)};
+        [F] -> {F, 0}
+    end.
+
 -compile({inline, [atom/1]}).
-atom(S) -> list_to_existing_atom(S).
+atom(S) -> list_to_atom(S).
+
+-compile({inline, [int/1]}).
+int(S) -> list_to_integer(S).
