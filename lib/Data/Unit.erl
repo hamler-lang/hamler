647b0e0df9e51b45f183c6c219e97a8fd06d3a32
EMQ-YangM <yangm@emqx.io>
2020-04-24 12:12:21 +0800

    Add Data.Unit module

---
+++ lib/Data/Unit.erl
@@ -0,0 +1,19 @@
+%%---------------------------------------------------------------------------
+%% |
+%% Module      :  Unit
+%% Copyright   :  (c) 2020 EMQ Technologies Co., Ltd.
+%% License     :  BSD-style (see the LICENSE file)
+%%
+%% Maintainer  :  Feng Lee, feng@emqx.io
+%%                Yang M, yangm@emqx.io
+%% Stability   :  experimental
+%% Portability :  portable
+%%
+%% The Tuple FFI Module.
+%%
+%%---------------------------------------------------------------------------
+-module('Unit').
+
+-export([unit/0]).
+
+unit() -> {'ok'}.
