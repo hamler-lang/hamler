e033b1c5946c3f422d9e18cf5078cbf6b4db61f3
Feng Lee <feng@emqx.io>
2020-04-25 10:51:21 +0800

    Add FFI functions for Monad, Show and Function modules

--- lib/Control/Monad.erl
+++ lib/Control/Monad.erl
@@ -16,8 +16,28 @@
 
 -export([ pureImpl/1
         , bindImpl/2
+        , mapListImpl/2
+        , applyListImpl/2
+        , bindListImpl/2
         ]).
 
+-type(mapFun() :: fun((A :: any()) -> B :: any())).
+
+-spec(pureImpl(any()) -> any()).
 pureImpl(X) -> X.
 
+-spec(bindImpl(any(), mapFun()) -> any()).
 bindImpl(X, F) -> F(X).
+
+-spec(mapListImpl(mapFun(), list(any())) -> list(any())).
+mapListImpl(F, L) -> lists:map(F, L).
+
+-spec(applyListImpl(list(mapFun()), list(any())) -> list(any())).
+applyListImpl(Funs, L) ->
+    lists:map(fun(A) ->
+                  lists:foldl(
+                    fun(F, X) -> F(X) end, A, Funs)
+              end, L).
+
+-spec(bindListImpl(list(any()), mapFun()) -> list(any())).
+bindListImpl(L, F) -> lists:flatten(lists:map(F, L)).
