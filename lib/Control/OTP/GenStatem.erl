5dbb5de99122dea57c71ff4a8af8c15cf6a5c500
EMQ-YangM <yangm@emqx.io>
2020-05-21 14:15:13 +0800

    Restore some files

---
+++ lib/Control/OTP/GenStatem.erl
@@ -0,0 +1,15 @@
+%%---------------------------------------------------------------------------
+%% |
+%% Module      :  GenStatem
+%% Copyright   :  (c) 2020 EMQ Technologies Co., Ltd.
+%% License     :  BSD-style (see the LICENSE file)
+%%
+%% Maintainer  :  Feng Lee, feng@emqx.io
+%%                Yang M, yangm@emqx.io
+%% Stability   :  experimental
+%% Portability :  portable
+%%
+%% The GenStatem FFI Module.
+%%
+%%---------------------------------------------------------------------------
+-module('GenStatem').
