8d47e2df2d93611cab06e6a9bd9aa3fd33e68dc1
Feng Lee <feng@emqx.io>
2020-05-24 16:55:18 +0800

    Implement an experimental 'GenServer' typeclass

--- lib/Control/OTP/GenServer.erl
+++ lib/Control/OTP/GenServer.erl
@@ -9,7 +9,20 @@
 %% Stability   :  experimental
 %% Portability :  portable
 %%
-%% The GenServer FFI Module.
+%% The GenServer FFI module.
 %%
 %%---------------------------------------------------------------------------
 -module('GenServer').
+
+-define(Mod, 'Control.OTP.GenServer.Behaviour').
+
+-export([startLink/2, call/2, cast/2]).
+
+startLink(Map, Env) ->
+    {ok, Pid} = gen_server:start_link(?Mod, [Map, Env], []),
+    Pid.
+
+call(Pid, Req) -> gen_server:call(Pid, Req).
+
+cast(Pid, Msg) -> gen_server:cast(Pid, Msg).
+
