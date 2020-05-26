e9edea934085813d7809e47fd5bfb8aea6dac333
Feng Lee <feng@emqx.io>
2020-05-26 16:11:02 +0800

    Add a new Ord instance for Atom

--- lib/Data/Atom.erl
+++ lib/Data/Atom.erl
@@ -15,12 +15,27 @@
 -module('Atom').
 
 -export([ atom/1
+        , existingAtom/1
         , toString/1
         , eqAtomImpl/2
+        , cmpAtomImpl/3
         ]).
 
+-spec(atom(string()) -> atom()).
 atom(S) -> list_to_atom(S).
 
+-spec(existingAtom(string()) -> atom()).
+existingAtom(S) -> list_to_existing_atom(S).
+
+-spec(toString(atom()) -> string()).
 toString(A) -> atom_to_list(A).
 
+-spec(eqAtomImpl(atom(), atom()) -> boolean()).
 eqAtomImpl(A1, A2) -> A1 =:= A2.
+
+cmpAtomImpl(LT, EQ, GT) ->
+    fun(A1, A2) when A1 < A2 -> LT;
+       (A1, A2) when A1 == A2 -> EQ;
+       (A1, A2) when A1 > A2 -> GT
+    end.
+
