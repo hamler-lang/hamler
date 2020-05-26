3a2e9cf3d16956d86356f01378518e1b0374bba0
Feng Lee <feng@emqx.io>
2020-05-26 16:13:24 +0800

    Add some foreign functions

--- lib/Control/Process.erl
+++ lib/Control/Process.erl
@@ -14,15 +14,32 @@
 %%---------------------------------------------------------------------------
 -module('Process').
 
--compile({no_auto_import, [spawn/2]}).
+-compile({no_auto_import, [node/1, spawn/2]}).
 
--export([ spawn/2
+-export([ node/1
+        , mod/1
+        , selfNode/0
+        , selfPid/0
+        , spawn/2
         , send/2
         , 'receive'/0
+        , receiveAfter/1
         , 'link'/1
         , 'monitor'/1
         , isProcessAlive/1
         ]).
+
+-spec(node(string()) -> atom()).
+node(S) -> list_to_atom(S).
+
+-spec(mod(string()) -> atom()).
+mod(S) -> list_to_existing_atom(S).
+
+-spec(selfNode() -> node()).
+selfNode() -> node().
+
+-spec(selfPid() -> pid()).
+selfPid() -> self().
 
 spawn(Fun, Arg) ->
     erlang:spawn(fun() -> Fun(Arg) end).
@@ -32,6 +49,11 @@ send(Pid, Msg) -> erlang:send(Pid, Msg).
 
 -spec('receive'() -> term()).
 'receive'() -> receive X -> X end.
+
+%% TODO: Fixme later:(
+-spec(receiveAfter(integer()) -> term()).
+receiveAfter(Timeout) ->
+    receive X -> X after Timeout -> ok end.
 
 -spec('link'(pid()) -> boolean()).
 'link'(Pid) -> erlang:link(Pid).
