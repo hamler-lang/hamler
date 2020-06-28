57baee4f0349f9062278f9dc927d9e839f4a8bf3
EMQ-YangM <yangm@emqx.io>
2020-06-28 17:23:07 +0800

    feat(Printf): put the implementation of the printf function in a separate module System.IO.Printf

---
+++ lib/System/IO/Printf.erl
@@ -0,0 +1,27 @@
+%%---------------------------------------------------------------------------
+%% |
+%% Module      :  IO
+%% Copyright   :  (c) 2020 EMQ Technologies Co., Ltd.
+%% License     :  BSD-style (see the LICENSE file)
+%%
+%% Maintainer  :  Feng Lee, feng@emqx.io
+%%                Yang M, yangm@emqx.io
+%% Stability   :  experimental
+%% Portability :  portable
+%%
+%% The Printf FFI module.
+%%
+%%---------------------------------------------------------------------------
+-module('Printf').
+
+-export([ print/1
+        , println/1
+        ]).
+
+
+-spec(print(string()) -> ok).
+print(S) -> io:format(S).
+
+-spec(println(string()) -> ok).
+println(S) -> io:format(S ++ "~n").
+
