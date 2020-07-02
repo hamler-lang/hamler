fb70bdc5e484c7f304e79c3ca14236bf924941f0
Feng Lee <feng@emqx.io>
2020-07-02 17:53:20 +0800

    feat(lib): improve the 'Supervisor' and 'GenServer' behaviours

--- lib/Control/Behaviour/Supervisor.erl
+++ lib/Control/Behaviour/Supervisor.erl
@@ -16,83 +16,91 @@
 
 -compile({no_auto_import, [error/1]}).
 
--export([ startSupervisor/2
-        , checkChildSpecs/1
+-export([ startSup/2
+        , startSupWith/3
+        , startSupWithGlobal/3
+        ]).
+
+-export([ checkChildSpecs/1
         , countChildren/1
         , deleteChild/2
         , getChildSpec/2
         , restartChild/2
         , startChild/2
         , terminateChild/2
-        , terminateChildByPid/2
+        , terminateChildBy/2
         , whichChildren/1
         ]).
 
 -define(MOD, 'Control.Behaviour.Supervisor.Proxy').
 
-%% TODO:
-startSupervisor(Class, Args) ->
-    {ok, Pid} = supervisor:start_link(?MOD, [Class, Args]),
-    Pid.
+startSup(Init, Args) ->
+  startRet(supervisor:start_link(?MOD, [Init, Args])).
+
+startSupWith(Name, Init, Args) ->
+  startRet(supervisor:start_link({local, Name}, ?MOD, [Init, Args])).
+
+startSupWithGlobal(Name, Init, Args) ->
+  startRet(supervisor:start_link({global, Name}, ?MOD, [Init, Args])).
 
 %% TODO: destruct childspecs
 checkChildSpecs(ChildSpecs) ->
-    case supervisor:check_childspecs(ChildSpecs) of
-        ok -> ok();
-        {error, Reason} ->
-            err(childSpecError(Reason))
+  case supervisor:check_childspecs(ChildSpecs) of
+    ok -> ok();
+    {error, Reason} ->
+      err(childSpecError(Reason))
     end.
 
-%% TODO:
 countChildren(SupRef) ->
-    maps:from_list(supervisor:count_children(supRef(SupRef))).
+  maps:from_list(supervisor:count_children(toErl(SupRef))).
 
-%% TODO:
 deleteChild(SupRef, Id) ->
-    case supervisor:delete_child(SupRef, Id) of
-        ok -> ok();
-        {error, Reason} ->
-            err(childError(Reason))
-    end.
+  case supervisor:delete_child(toErl(SupRef), Id) of
+    ok -> ok();
+    {error, Reason} ->
+      err(childError(Reason))
+  end.
 
-%% TODO:
 getChildSpec(SupRef, Id) ->
-    case supervisor:get_childspec(supRef(SupRef), Id) of
-        {ok, ChildSpec} ->
-            just(childSpecRecord(ChildSpec));
-        {error, not_found} -> nothing()
-    end.
+  case supervisor:get_childspec(toErl(SupRef), Id) of
+    {ok, ChildSpec} ->
+      just(childSpecRecord(ChildSpec));
+    {error, not_found} -> nothing()
+  end.
 
-%% TODO:
 restartChild(SupRef, Id) ->
-    case supervisor:restartChild(supRef(SupRef), Id) of
-        {ok, Child} ->
-            ok(childRecord(Child));
-        {error, Reason} ->
-            err(childError(Reason))
-    end.
+  case supervisor:restartChild(toErl(SupRef), Id) of
+    {ok, Child} ->
+        ok(childRecord(Child));
+    {error, Reason} ->
+      err(childError(Reason))
+  end.
 
 startChild(SupRef, ChildSpec) ->
-    case supervisor:start_child(supRef(SupRef), ChildSpec) of
-        {ok, ChildPid} -> ok(ChildPid);
-        {error, Reason} ->
-            err(childError(Reason))
-    end.
+  case supervisor:start_child(toErl(SupRef), ChildSpec) of
+    {ok, ChildPid} ->
+      ok(ChildPid);
+    {error, Reason} ->
+      err(childError(Reason))
+  end.
 
-terminateChildByPid(SupRef, Pid) ->
-    terminateChild(SupRef, Pid).
+terminateChild(SupRef, Id) ->
+  case supervisor:terminate_child(toErl(SupRef), Id) of
+    ok -> ok();
+    {error, Reason} ->
+      err(childError(Reason))
+  end.
 
-%% TODO:
-terminateChild(SupRef, ChildId) ->
-    case supervisor:terminate_child(supRef(SupRef), ChildId) of
-        ok -> ok(ok);
-        {error, Reason} ->
-            err(childError(Reason))
-    end.
+terminateChildBy(SupRef, Pid) ->
+  terminateChild(SupRef, Pid).
 
-%% TODO:
+%% TODO: fixme later...
 whichChildren(SupRef) ->
-    [Id || {Id, _, _, _} <- supervisor:which_children(supRef(SupRef))].
+  [Id || {Id, _, _, _} <- supervisor:which_children(toErl(SupRef))].
+
+%%---------------------------------------------------------------------------
+%% | Internal functions
+%%---------------------------------------------------------------------------
 
 -compile({inline, [ok/0, ok/1]}).
 ok() -> {'Ok', ok}.
@@ -107,11 +115,14 @@ nothing() -> {'Nothing'}.
 -compile({inline, [just/1]}).
 just(Result) -> {'Just', Result}.
 
-supRef({'SupName', Name}) -> Name;
-supRef({'SupNameOn', Name, Node}) -> {Name, Node};
-supRef({'SupGlobal', Name}) -> {global, Name};
-supRef({'SupNameVia', Module, Name}) -> {via, Module, Name};
-supRef({'SupPid', Pid}) -> Pid.
+toErl({'SupName', Name}) -> Name;
+toErl({'SupNameAt', Name, Node}) -> {Name, Node};
+toErl({'SupGlobal', Name}) -> {global, Name};
+toErl({'SupPid', Pid}) -> Pid.
+
+startRet({ok, Pid}) -> {'StartOk', Pid};
+startRet(ignore) -> {'StartIgnore'};
+startRet({error, Reason}) -> {'StartError', Reason}.
 
 %% TODO:...
 childRecord(Child) -> Child.
