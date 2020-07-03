a2184ff98e80b6ec53e7105077e37c930d926857
Feng Lee <feng@emqx.io>
2020-07-03 17:37:31 +0800

    feat(lib): add Foreign folder

---
+++ lib/Foreign/Curry.erl
@@ -0,0 +1,24 @@
+%%---------------------------------------------------------------------------
+%% |
+%% Module      :  Curry
+%% Copyright   :  (c) 2020 EMQ Technologies Co., Ltd.
+%% License     :  BSD-style (see the LICENSE file)
+%%
+%% Maintainer  :  Feng Lee, feng@emqx.io
+%%                Yang M, yangm@emqx.io
+%% Stability   :  experimental
+%% Portability :  portable
+%%
+%% The Curry module.
+%%
+%%---------------------------------------------------------------------------
+-module('Curry').
+
+-export([uncurry/2]).
+
+uncurry(Fun, [H|T]) -> uncurry(Fun(H), T);
+uncurry(Ret, []) -> Ret.
+
+%%curry(M, F, A, 0) -> erlang:apply(M, F, lists:reverse(A));
+%%curry(M, F, A, N) -> fun(X) -> curry(M, F, [X|A], N-1) end.
+
