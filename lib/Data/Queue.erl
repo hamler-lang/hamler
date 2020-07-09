92a52ec1f6a107393aebc142ce2b2e75cd2bc219
Feng Lee <feng@emqx.io>
2020-07-09 21:42:46 +0800

    feat(lib/Data): add Array, Queue, and Base64 modules

---
+++ lib/Data/Queue.erl
@@ -0,0 +1,33 @@
+%%---------------------------------------------------------------------------
+%% |
+%% Module      :  Queue
+%% Copyright   :  (c) 2020 EMQ Technologies Co., Ltd.
+%% License     :  BSD-style (see the LICENSE file)
+%%
+%% Maintainer  :  Feng Lee, feng@emqx.io
+%%                Yang M, yangm@emqx.io
+%% Stability   :  experimental
+%% Portability :  portable
+%%
+%% The Queue FFI module.
+%%
+%%---------------------------------------------------------------------------
+-module('Queue').
+
+-export([daeh/1, drop/1, out/1, outR/1, peek/1, peekR/1]).
+
+daeh(Q) ->
+  try queue:daeh(Q) of
+    E -> {'Just', E}
+  catch error:empty ->
+    {'Nothing'}
+  end.
+
+%% TODO: Fixme later.
+
+drop(Q) -> queue:drop(Q).
+out(Q) -> queue:out(Q).
+outR(Q) -> queue:out_r(Q).
+peek(Q) -> queue:peek(Q).
+peekR(Q) -> queue:peek_r(Q).
+
