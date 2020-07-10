7e6f09a5fd6f97d4ac8569dac9069dd14c2e79ff
Feng Lee <feng@emqx.io>
2020-07-10 16:47:22 +0800

    feat(Pid): make Pid instance of Eq and Ord

--- lib/Data/Pid.erl
+++ lib/Data/Pid.erl
@@ -14,7 +14,20 @@
 %%---------------------------------------------------------------------------
 -module('Pid').
 
--export([showPidImpl/1]).
+-export([eqPidImpl/2, cmpPidImpl/3, showPidImpl/1]).
+
+-spec(eqPidImpl(pid(), pid()) -> boolean()).
+eqPidImpl(Pid1, Pid2) -> Pid1 == Pid2.
+
+cmpPidImpl(LT, EQ, GT) ->
+  fun(Pid1) ->
+      fun(Pid2) ->
+          if Pid1 > Pid2 -> GT;
+             Pid1 < Pid2 -> LT;
+             true -> EQ
+          end
+      end
+  end.
 
 -spec(showPidImpl(pid()) -> string()).
 showPidImpl(Pid) -> pid_to_list(Pid).
