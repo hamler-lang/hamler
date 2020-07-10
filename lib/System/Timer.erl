e1dbde57a91bcb7cdcfd99e32e208cfd600093a7
Feng Lee <feng@emqx.io>
2020-07-10 14:59:14 +0800

    feat(Timer): add an inital Timer module

---
+++ lib/System/Timer.erl
@@ -0,0 +1,35 @@
+%%---------------------------------------------------------------------------
+%% |
+%% Module      :  Timer
+%% Copyright   :  (c) 2020 EMQ Technologies Co., Ltd.
+%% License     :  BSD-style (see the LICENSE file)
+%%
+%% Maintainer  :  Feng Lee, feng@emqx.io
+%%                Yang M, yangm@emqx.io
+%% Stability   :  experimental
+%% Portability :  portable
+%%
+%% The Timer FFI module.
+%%
+%%---------------------------------------------------------------------------
+-module('Timer').
+
+-export([readTimer/1]).
+
+readTimer(TimerRef) ->
+  case erlang:read_timer(TimerRef) of
+    false -> {'Nothing'};
+    Time -> {'Just', Time}
+  end.
+
+%% erlang:cancel_timer(TimerRef) -> Result
+%% erlang:cancel_timer(TimerRef, Options) -> Result | ok
+
+%% erlang:read_timer(TimerRef) -> Result
+%% erlang:read_timer(TimerRef, Options) -> Result | ok
+
+%% erlang:send_after(Time, Dest, Msg) -> TimerRef
+%% erlang:send_after(Time, Dest, Msg, Options) -> TimerRef
+
+%% erlang:start_timer(Time, Dest, Msg) -> TimerRef
+%% erlang:start_timer(Time, Dest, Msg, Options) -> TimerRef
