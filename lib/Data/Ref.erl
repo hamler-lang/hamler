68828cffeda227d1091c7eea89a335d58893743d
Feng Lee <feng@emqx.io>
2020-07-10 16:47:43 +0800

    feat(Ref): make Ref instance of Eq and Ord

--- lib/Data/Ref.erl
+++ lib/Data/Ref.erl
@@ -15,11 +15,26 @@
 -module('Ref').
 
 -export([ makeRef/0
+        , eqRefImpl/2
+        , cmpRefImpl/3
         , showRefImpl/1
         ]).
 
 -spec(makeRef() -> reference()).
 makeRef() -> erlang:make_ref().
+
+-spec(eqRefImpl(reference(), reference()) -> boolean()).
+eqRefImpl(Ref1, Ref2) -> Ref1 == Ref2.
+
+cmpRefImpl(LT, EQ, GT) ->
+  fun(Ref1) ->
+      fun(Ref2) ->
+          if Ref1 > Ref2 -> GT;
+             Ref1 < Ref2 -> LT;
+             true -> EQ
+          end
+      end
+  end.
 
 -spec(showRefImpl(reference()) -> string()).
 showRefImpl(Ref) -> ref_to_list(Ref).
