fb264ad167c910b0269fea6641da41b529c1b61c
Feng Lee <feng@emqx.io>
2020-07-17 09:37:24 +0800

    feat(NetAdm): implement NetAdm module

---
+++ lib/Control/Distributed/NetAdm.erl
@@ -0,0 +1,44 @@
+%%---------------------------------------------------------------------------
+%% |
+%% Module      :  NetAdm
+%% Copyright   :  (c) 2020 EMQ Technologies Co., Ltd.
+%% License     :  BSD-style (see the LICENSE file)
+%%
+%% Maintainer  :  Feng Lee, feng@emqx.io
+%%                Yang M, yangm@emqx.io
+%% Stability   :  experimental
+%% Portability :  portable
+%%
+%% The NetAdm FFI module.
+%%
+%%---------------------------------------------------------------------------
+-module('NetAdm').
+
+-include("../../Foreign.hrl").
+
+-export([ localhost/0
+        , names/0
+        , namesAt/1
+        , ping/1
+        ]).
+
+localhost() -> ?IO(net_adm:localhost()).
+
+names() ->
+  ?IO(case net_adm:names() of
+        {ok, List} -> List;
+        {error, Reason} -> error(Reason)
+      end).
+
+namesAt(Host) ->
+  ?IO(case net_adm:names(Host) of
+        {ok, List} -> List;
+        {error, Reason} -> error(Reason)
+      end).
+
+ping(Node) ->
+  ?IO(case net_adm:ping(list_to_atom(Node)) of
+        pong -> true;
+        pang -> false
+      end).
+
--- lib/Control/Distributed/Node.erl
+++ lib/Control/Distributed/Node.erl
@@ -16,27 +16,57 @@
 
 -include("../../Foreign.hrl").
 
--export([ nodesOf/1
+-export([ nodes/0
+        , nodesOf/1
+        , selfNode/0
+        , nodeOfPid/1
+        , nodeOfPort/1
+        , nodeOfRef/1
         , disconnect/1
         , getCookie/0
         , setCookie/2
+        , isAlive/0
+        , monitorNode/2
         ]).
 
+nodes() ->
+  ?IO([name(N) || N <- erlang:nodes()]).
+
 nodesOf(Type) ->
-  ?IO(erlang:nodes(toErl(Type))).
+  ?IO([name(N) || N <- erlang:nodes(toErl(Type))]).
+
+selfNode() ->
+  ?IO(name(erlang:node())).
+
+nodeOfPid(Pid) ->
+  ?IO(name(erlang:node(Pid))).
+
+nodeOfPort(Port) ->
+  ?IO(name(erlang:node(Port))).
+
+nodeOfRef(Ref) ->
+  ?IO(name(erlang:node(Ref))).
 
 disconnect(Node) ->
-  ?IO(case erlang:disconnect_node(Node) of
-        true    -> {'Just', true};
-        false   -> {'Just', false};
-        ignored -> {'Nothing'}
+  ?IO(case erlang:disconnect_node(atom(Node)) of
+        true    -> ?Just(true);
+        false   -> ?Just(false);
+        ignored -> ?Nothing
       end).
 
 getCookie() ->
-  ?IO(atom_to_list(erlang:get_cookie())).
+  ?IO(name(erlang:get_cookie())).
 
 setCookie(Node, Cookie) ->
-  ?IO(erlang:set_cookie(Node, list_to_atom(Cookie))).
+  ?IO(erlang:set_cookie(atom(Node), atom(Cookie))).
+
+isAlive() -> ?IO(erlang:is_alive()).
+
+monitorNode(Node, Flag) ->
+  ?IO(erlang:monitor_node(atom(Node), Flag)).
+
+atom(S) -> list_to_atom(S).
+name(N) -> atom_to_list(N).
 
 toErl({'VisibleNode'}) -> visible;
 toErl({'HiddenNode'}) -> hidden;
