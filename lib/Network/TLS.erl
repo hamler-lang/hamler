e909eab48cd56674face3c0fd91f09ee35e538ac
Feng Lee <feng@emqx.io>
2020-07-22 19:02:54 +0800

    feat(Network): introduce a new design for network lib

---
+++ lib/Network/TLS.erl
@@ -0,0 +1,15 @@
+%%---------------------------------------------------------------------------
+%% |
+%% Module      :  TLS
+%% Copyright   :  (c) 2020 EMQ Technologies Co., Ltd.
+%% License     :  BSD-style (see the LICENSE file)
+%%
+%% Maintainer  :  Feng Lee, feng@emqx.io
+%%                Yang M, yangm@emqx.io
+%% Stability   :  experimental
+%% Portability :  portable
+%%
+%% The TLS FFI module.
+%%
+%%---------------------------------------------------------------------------
+-module('TLS').
