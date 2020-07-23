9c6c571577b12da858c5b78b2e559979dac413cf
Feng Lee <feng@emqx.io>
2020-07-23 13:16:57 +0800

    feat(OS): add more functions for OS module

--- lib/System/OS.erl
+++ lib/System/OS.erl
@@ -16,13 +16,31 @@
 
 -include("../Foreign.hrl").
 
--export([ getPid/0
+-export([ findExe/1
+        , findExeIn/2
+        , getPid/0
         , osName/0
         , osFamily/0
         , runCmd/1
         , setSignal/2
+        , systemTime/0
+        , systemTimeIn/1
+        , perfCounter/0
+        , perfCounterIn/1
         , version/0
         ]).
+
+findExe(Name) ->
+  ?IO(case os:find_executable(Name) of
+        false -> ?Nothing;
+        Filename -> ?Just(Filename)
+      end).
+
+findExeIn(Name, Path) ->
+  ?IO(case os:find_executable(Name, Path) of
+        false -> ?Nothing;
+        Filename -> ?Just(Filename)
+      end).
 
 getPid() ->
   ?IO(list_to_integer(os:getpid())).
@@ -33,11 +51,22 @@ osName() ->
 osFamily() ->
   ?IO(atom_to_list(element(1, os:type()))).
 
-setSignal(Signal, Option) ->
-  ?IO(os:set_signal(destruct(Signal), destruct(Option))).
+runCmd(Command) -> ?IO(os:cmd(Command)).
 
-runCmd(Command) ->
-  ?IO(os:cmd(Command)).
+setSignal(Signal, Option) ->
+  ?IO(os:set_signal(unwrap(Signal), unwrap(Option))).
+
+systemTime() ->
+  ?IO(os:system_time()).
+
+systemTimeIn(Unit) ->
+  ?IO(os:system_time(unwrap(Unit))).
+
+perfCounter() ->
+  ?IO(os:perf_counter()).
+
+perfCounterIn(Unit) ->
+  ?IO(os:perf_counter(unwrap(Unit))).
 
 -spec(version() -> string()).
 version() ->
@@ -47,18 +76,23 @@ version() ->
         VersionString -> VersionString
       end).
 
-destruct({'Sighup'})  -> sighup;
-destruct({'Sigquit'}) -> sigquit;
-destruct({'Sigabrt'}) -> sigabrt;
-destruct({'Sigalrm'}) -> sigalrm;
-destruct({'Sigterm'}) -> sigterm;
-destruct({'Sigusr1'}) -> sigusr1;
-destruct({'Sigusr2'}) -> sigusr2;
-destruct({'Sigchld'}) -> sigchld;
-destruct({'Sigstop'}) -> sigstop;
-destruct({'Sigtstp'}) -> sigtstp;
+unwrap({'Second'}) -> second;
+unwrap({'Millisecond'}) -> millisecond;
+unwrap({'Microsecond'}) -> microsecond;
+unwrap({'Nanosecond'}) -> nanosecond;
+unwrap({'Native'}) -> native;
+unwrap({'PerfCounter'}) -> perf_counter;
 
-destruct({'SigDefault'}) -> default;
-destruct({'SigHandle'})  -> handle;
-destruct({'SigIgnore'})  -> ignore.
-
+unwrap({'Sighup'})  -> sighup;
+unwrap({'Sigquit'}) -> sigquit;
+unwrap({'Sigabrt'}) -> sigabrt;
+unwrap({'Sigalrm'}) -> sigalrm;
+unwrap({'Sigterm'}) -> sigterm;
+unwrap({'Sigusr1'}) -> sigusr1;
+unwrap({'Sigusr2'}) -> sigusr2;
+unwrap({'Sigchld'}) -> sigchld;
+unwrap({'Sigstop'}) -> sigstop;
+unwrap({'Sigtstp'}) -> sigtstp;
+unwrap({'SigDefault'}) -> default;
+unwrap({'SigHandle'})  -> handle;
+unwrap({'SigIgnore'})  -> ignore.
