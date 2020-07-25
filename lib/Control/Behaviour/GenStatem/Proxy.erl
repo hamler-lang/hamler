7a215851b11ed563e50b282ccc1f551bc5a137c0
Feng Lee <feng@emqx.io>
2020-07-25 18:18:07 +0800

    feat(GenStatem): improve the typecass of GenStatem behaviour

--- lib/Control/Behaviour/GenStatem/Proxy.erl
+++ lib/Control/Behaviour/GenStatem/Proxy.erl
@@ -14,6 +14,8 @@
 %%---------------------------------------------------------------------------
 -module('Proxy').
 
+-include("../../../Foreign.hrl").
+
 -behaviour(gen_statem).
 
 -export([ init/1
@@ -23,21 +25,44 @@
         , code_change/4
         ]).
 
--record(proxy, {handleEvent, state}).
+-record(proxy, {handleEvent, data}).
 
-init([#{handleEvent := handleEvent}, Init]) ->
-  {ok, idle, #proxy{handleEvent = handleEvent, state = Init()}}.
+init([Class, Init]) ->
+  case Init() of
+    {'InitOk', State, Data} ->
+      {ok, State, init_ok(Class, Data)};
+    {'InitOkHib', State, Data} ->
+      {ok, State, init_ok(Class, Data), hibernate};
+    {'InitIgnore'} ->
+      ignore;
+    {'InitStop', Reason} ->
+      {stop, Reason}
+  end.
+
+init_ok(#{handleEvent := HandleEvent}, Data) ->
+  #proxy{handleEvent = HandleEvent, data = Data}.
 
 callback_mode() -> handle_event_function.
 
-handle_event(EventType, Event, State, Data) ->
-  %% Ignore all other events
+handle_event(EventType, Event, State,
+             Proxy = #proxy{handleEvent = HandleEvent, data = Data}) ->
   io:format("EventType: ~p, Event: ~p~n", [EventType, Event]),
-  {next_state, State, Data}.
+  Args = [wrap(EventType), Event, State, Data],
+  case ?RunIO('Curry':apply(HandleEvent, Args)) of
+    {'Keep', NData} ->
+      {keep_state, Proxy#proxy{data = NData}};
+    {'Next', NState, NData} ->
+      {next_state, NState, Proxy#proxy{data = NData}};
+    {'Repeat', NData} ->
+      {repeat_state, Proxy#proxy{data = NData}}
+  end.
 
 terminate(_Reason, _State, _Data) ->
   void.
 
 code_change(_Vsn, State, Data, _Extra) ->
   {ok, State, Data}.
+
+wrap({call, From}) -> {'Call', From};
+wrap(cast) -> {'Cast'}.
 
