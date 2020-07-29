17946a25a85f9aea0e13d97e618267f800a39320
Feng Lee <feng@emqx.io>
2020-07-29 18:25:12 +0800

    feat(Application): add Config & Resource APIs

---
+++ lib/Control/Application/Resource.erl
@@ -0,0 +1,30 @@
+%%---------------------------------------------------------------------------
+%% |
+%% Module      :  Resource
+%% Copyright   :  (c) 2020 EMQ Technologies Co., Ltd.
+%% License     :  BSD-style (see the LICENSE file)
+%%
+%% Maintainer  :  Feng Lee, feng@emqx.io
+%%                Yang M, yangm@emqx.io
+%% Stability   :  experimental
+%% Portability :  portable
+%%
+%% The Application Resource FFI.
+%%
+%%---------------------------------------------------------------------------
+-module('Resource').
+
+-include("../../Foreign.hrl").
+
+-export([getAppSpec/1]).
+
+-import(proplists, [get_value/3]).
+
+getAppSpec(App) ->
+  ?IO(case application:get_all_key(App) of
+        {ok, Keys} -> specRec(App, Keys);
+        undefined -> error('AppNotFound')
+      end).
+
+specRec(App, Keys) ->
+  maps:put(name, App, maps:from_list(Keys)).
