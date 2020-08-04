826c1a87ed6040f50eda353655d754fb3b6a9f3e
niltok <niltok@163.com>
2020-08-04 15:52:03 +0800

    feat(Array): add run

--- lib/Data/Regex.erl
+++ lib/Data/Regex.erl
@@ -3,6 +3,7 @@
 -export([compile/2
         , inspect/1
         , replace/4
+        , run/4
         ]).
 
 match_newline(X) -> case X of
@@ -50,12 +51,8 @@ match_runtime({X}) -> case X of
   'NotEmptyStart' -> notempty_start
 end.
 
-trans_runtime([]) -> [];
-trans_runtime([ X | Xs ]) -> 
-  [match_runtime(X) | trans_runtime(Xs)].
-
 compile(S, Opt) -> case re:compile(S, trans_compile(Opt)) of
-  {ok, X} -> {'Ok', X};
+  {ok, X} -> {'Ok', {X}};
   {error, X} -> {'Error', X}
 end.
 
@@ -70,3 +67,62 @@ trans_replace([{'ReplaceRuntimeOption', X} | Xs]) ->
 
 replace(Sub, RE, Rep, Opt) ->
   re:replace(Sub, RE, Rep, trans_replace([{return, list} | Opt])).
+
+trans_value_list([]) -> [];
+trans_value_list([{_, {X}} | Xs]) ->
+  [X | trans_value_list(Xs)].
+
+match_value_spec({'ValueList', {X}}) ->
+  trans_value_list(X);
+match_value_spec({X}) -> case X of
+  'All' -> all;
+  'AllButFirst' -> all_but_first;
+  'AllNames' -> all_name;
+  'First' -> first;
+  'None' -> none
+end.
+
+match_capture_type({X}) -> case X of
+  'CaptureIndex' -> index;
+  'CaptureList' -> list;
+  'CaptureBinary' -> binary
+end.
+
+trans_run([]) -> [];
+trans_run([{'RunCompileOption', X} | Xs]) ->
+  [match_compile(X) | trans_run(Xs)];
+trans_run([{'RunRuntimeOption', X} | Xs]) ->
+  [match_runtime(X) | trans_run(Xs)];
+trans_run([{'ReportErrors'} | Xs]) ->
+  [report_errors | trans_run(Xs)];
+trans_run([{'Capture', {X, Y}} | Xs]) ->
+  [{capture, match_value_spec(X), match_capture_type(Y)} | trans_run(Xs)].
+
+match_run_err(X) -> case X of
+  match_limit -> {'MatchLimit'};
+  match_limit_recursion -> {'MatchLimitRecursion'};
+  {compile, X} -> {'CompileErr', X}
+end.
+
+trans_captured([]) -> [];
+trans_captured([{X, Y} | Xs]) 
+  when is_integer(X) and is_integer(Y) ->
+    [{'DataIndex', {X, Y}} | trans_captured(Xs)];
+trans_captured([X | Xs]) when is_binary(X) ->
+  [{'DataBinary', {X}} | trans_captured(Xs)];
+trans_captured([[C | Cs] | Xs]) when is_integer(C) ->
+  [{'DataString', {[C | Cs]}} | trans_captured(Xs)];
+trans_captured([X | Xs]) when is_list(X) ->
+  [{'Datas', {trans_captured(X)}} | trans_captured(Xs)];
+trans_captured([{error, X, Y} | Xs]) ->
+  [{'DataErr', {X, Y}} | trans_captured(Xs)];
+trans_captured([{incomplete, X, Y} | Xs]) ->
+  [{'DataIncomplete', {X, Y}} | trans_captured(Xs)].
+
+run(Sub, RE, Rep, Opt) -> 
+  case re:run(Sub, RE, Rep, trans_run(Opt)) of
+    {match, X} -> {'Match', {trans_captured(X)}};
+    match -> {'Match', {[]}};
+    nomatch -> {'NoMatch'};
+    {error, X} -> match_run_err(X)
+end.
