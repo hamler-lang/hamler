56fda3455e39225f1f624ba04ae0ffdc4fc69dd7
EMQ-YangM <yangm@emqx.io>
2020-08-18 17:09:11 +0800

    fix(Set): fix fold function

--- lib/Data/Set.erl
+++ lib/Data/Set.erl
@@ -14,7 +14,12 @@
 %%---------------------------------------------------------------------------
 -module('Set').
 
--export([singleton/1]).
+-export([ singleton/1
+        , fold/3
+        ]).
 
 -spec(singleton(term()) -> sets:set()).
 singleton(A) -> sets:add_element(A, sets:new()).
+
+fold(Fun, B0, Set) ->
+    sets:fold(fun(A, B) -> (Fun(A))(B) end, B0, Set).
