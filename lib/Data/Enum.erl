9b8c0cb59d6e653d0d398200573c61b9b1d9e390
niltok <niltok@163.com>
2020-09-02 12:05:50 +0800

    feat(Enum): Bool & Float

--- lib/Data/Enum.erl
+++ lib/Data/Enum.erl
@@ -14,11 +14,23 @@
 %%---------------------------------------------------------------------------
 -module('Enum').
 
--export([ enumCharRange/2
+-export([ enumCharSucc/1
+        , enumCharPred/1
+        , enumCharRange/2
         , enumCharRangeStep/3
+        , enumIntegerSucc/1
+        , enumIntegerPred/1
         , enumIntegerRange/2
         , enumIntegerRangeStep/3
+        , enumFloatSucc/1
+        , enumFloatPred/1
+        , enumFloatRange/2
+        , enumFloatRangeStep/3
         ]).
+
+enumCharSucc(X) -> X + 1.
+
+enumCharPred(X) -> X - 1.
 
 enumCharRange(Start, Stop) when Stop >= Start ->
   range(Start, Stop, 1);
@@ -28,6 +40,10 @@ enumCharRange(Start, Stop) when Stop < Start ->
 enumCharRangeStep(Start, Next, Stop) ->
   range(Start, Stop, Next-Start).
 
+enumIntegerSucc(X) -> X + 1.
+
+enumIntegerPred(X) -> X - 1.
+
 enumIntegerRange(Start, Stop) when Stop >= Start ->
   range(Start, Stop, 1);
 enumIntegerRange(Start, Stop) when Stop < Start ->
@@ -35,6 +51,24 @@ enumIntegerRange(Start, Stop) when Stop < Start ->
 
 enumIntegerRangeStep(Start, Next, Stop) ->
   range(Start, Stop, Next-Start).
+
+enumFloatSucc(X) -> X + 1.
+
+enumFloatPred(X) -> X - 1.
+
+enumFloatRange(Start, Stop) when Stop >= Start ->
+  floatRange(Start, Stop, 1);
+enumFloatRange(Start, Stop) when Stop < Start ->
+  floatRange(Start, Stop, -1).
+
+enumFloatRangeStep(Start, Next, Stop) ->
+  floatRange(Start, Stop, Next-Start).
+
+floatRange(_, _, 0.0) -> error("bad argument");
+floatRange(Start, Stop, Step) 
+  when (Step > 0.0) and (Start > Stop) or (Step < 0.0) and (Start < Stop) -> [];
+floatRange(Start, Stop, Step) -> 
+  [Start | floatRange(Start + Step, Stop, Step)].
 
 -compile({inline, [range/3]}).
 range(Start, Stop, Step) ->
