e9dd7b3eb1dfbb48f7845dc7e1c39fc21541e408
niltok <niltok@163.com>
2021-01-30 20:16:43 +0800

    feat(Data.Binary): funs

--- lib/Data/Binary.erl
+++ lib/Data/Binary.erl
@@ -14,3 +14,30 @@
 %%---------------------------------------------------------------------------
 -module('Binary').
 
+-export([
+    split/3,
+    replace/4,
+    matches/3
+]).
+
+splitOptions(Options) -> case Options of
+    { 'SplitGlobal' } -> global;
+    { 'SplitTrim' } -> trim;
+    { 'SplitTrimAll' } -> trim_all;
+    { 'SplitScope', S, L } -> {scope, {S, L}}
+end.
+
+split(Subject, Pattern, Options) -> 
+    binary:split(Subject, Pattern, list:map(fun splitOptions/1, Options)).
+
+replaceOptions(Options) -> case Options of
+    { 'ReplaceGlobal' } -> global;
+    { 'ReplaceScope', S, L } -> {scope, {S, L}};
+    { 'InsertReplaced', InsPos } -> {insert_replaced, InsPos}
+end.
+
+replace(Subject, Pattern, Replacement, Options) ->
+    binary:split(Subject, Pattern, Replacement, list:map(fun replaceOptions/1, Options)).
+
+matches(Subject, Pattern, Options) ->
+    binary:matches(Subject, Pattern, list:map(fun (X) -> {scope, X} end, Options)).
