bbc975f64a8a7778eb9a97adf892dd0934fbbd74
EMQ-YangM <yangm@emqx.io>
2021-05-25 16:01:16 +0800

    chore(build): update hamler home

--- get-hamler-home.sh
+++ get-hamler-home.sh
@@ -17,8 +17,8 @@ else
     HAMLER_HOME="$HOME/.hamler"
 fi
 
-if [ -z "$(echo $PATH |grep -o $HAMLER_HOME)" ]; then
+if [ -z "$(echo $PATH |grep -o $HAMLER_HOME/bin)" ]; then
     rc_file="$HOME/.$(basename $(echo $SHELL))rc"
-    echo "export PATH=$HAMLER_HOME:\$PATH" >> $rc_file
+    echo "export PATH=$HAMLER_HOME/bin:\$PATH" >> $rc_file
 fi
 echo $HAMLER_HOME
